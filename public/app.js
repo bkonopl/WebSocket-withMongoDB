908e5a058b4efab8fd7527e90b11aa4e436043b4
theboatman <egorkuchar@gmail.com>
2021-06-01 00:41:18 +0300

    its ugly but work

--- public/app.js
+++ public/app.js
@@ -2,27 +2,65 @@ const status = document.getElementById('status');
 const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
+
+const fotoId = document.getElementById('foto')
+const firstNameId = document.getElementById('firstName')
+const secondNameId = document.getElementById('secondName')
+const lastNameId = document.getElementById('lastName')
+const facultyId = document.getElementById('faculty')
+const groupId = document.getElementById('group')
+const dateId = document.getElementById('date')
+
+
 let ws
 let nickname
-while(1) {
-    nickname = prompt('Как вас зовут?')
-    if(nickname !== '') {
+// while(1) {
+//     nickname = prompt('Как вас зовут?')
+//     if(nickname !== '') {
         start()
-        break
-    }
-
-}
+//         break
+//     }
+//
+// }
 function start() {
-    ws = new WebSocket('ws://192.168.0.110:3000')
+    ws = new WebSocket('ws://192.168.0.104:3000')
 }
 function setStatus(value) {
     status.innerHTML = value;
 }
 
-function printMessage(value) {
+function printMessage(data) {
+    console.log('start')
+    console.log(data)
+    console.log('end')
+
+    fotoId.innerHTML = `<img src="${data.img}"/>`
+
+    firstNameId.innerHTML = data.firstName
+    secondNameId.innerHTML = data.secondName
+    lastNameId.innerHTML = data.lastName
+
+    facultyId.innerHTML = data.faculty
+    groupId.innerHTML = data.group
+    dateId.innerHTML = data.date
+
+    console.log(data.firstName)
+    console.log(data.secondName)
+    console.log(data.lastName)
+
+    console.log(data.room)
+    console.log(data.img)
+
+    console.log(data.faculty)
+    console.log(data.group)
+    console.log(data.date)
+
+    console.log('end')
+
+    /*
     const li = document.createElement('li');
     li.innerHTML = value;
-    messages.appendChild(li);
+    */
 }
 
 form.addEventListener('submit', event => {
@@ -38,5 +76,5 @@ ws.onopen = () => setStatus('ONLINE')
 ws.onclose = () => setStatus('disc')
 
 ws.onmessage = response => {
-    printMessage(response.data)
+    printMessage(JSON.parse(response.data))
 }
