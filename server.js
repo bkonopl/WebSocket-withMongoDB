71806254cf7153e60464bb53bf9dc108cfba1ede
bkonopl <bkonopli@gmail.com>
2021-06-01 11:29:27 +0300

    Fix: try catch

--- server.js
+++ server.js
@@ -10,27 +10,27 @@ async function start() {
             useNewUrlParser: true,
             useFindAndModify: true
         });
-    // console.log(await Resident.findById('60b361ff5ca4ea8fb14e671d').lean())
     async function drop(id){
         let data = await Resident.findById(id).lean()
-        // console.log(data)
         return data
     }
     const server = new WebSocket.Server({port: 3000})
     server.on('connection', (ws)  => {
         console.log('New Connection')
         ws.on('message', data => {
-            let {msg: id} =  JSON.parse(data)
-            // console.log(id)
-            drop(id).then((user) => {
-                    console.log(user)
-                    server.clients.forEach(client => {
-                        if (client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify(user))
-                        }
-                    })
-                }
-            )
+            try {
+                let {msg: id} =  JSON.parse(data)
+                drop(id).then((user) => {
+                        server.clients.forEach(client => {
+                            if (client.readyState === WebSocket.OPEN) {
+                                client.send(JSON.stringify(user))
+                            }
+                        })
+                    }
+                )
+            } catch(err) {
+                console.log(err)
+            }
         })
     })
 
